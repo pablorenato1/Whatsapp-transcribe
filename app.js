83ab278f82c6777dac5fd51aa14cfa682ebb345e
pablorenato1 <56098864+pablorenato1@users.noreply.github.com>
2022-10-06 20:07:00 -0300

    Final Version
    The code are commented and more clean to understand

--- app.js
+++ app.js
@@ -1,100 +1,80 @@
 
-const { MessageMedia, Client, LocalAuth } = require('whatsapp-web.js');
-const { writeFile, writeFileSync, createReadStream, createWriteStream, pipe } = require('fs');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const { v4: uuidv4 } = require('uuid');
 const qrcode = require('qrcode-terminal');
 const { setTimeout } = require('timers');
-
-const axios = require('axios');
-
 const fetch = require('node-fetch');
 
 
-/* Amazon API Gateway */
+// API gateway parameter
+ const urlGatewayDeployed = "https://keg43ehf5h.execute-api.us-east-1.amazonaws.com/dev";
+ const bucketInput = "audio-input-raw"
+ const bucketOutput = "transcribe-file-output"
 
-// End of Amazon part
+// Functions part
+async function getMethod(tries, name, msg) { // Recursive Function
+  
+  if(tries > 25) { // The limit of tries
+    return console.log("Failed to find the response of AWS");
+  }
+  await new Promise(resolve => setTimeout(resolve, 8000));
+  // Trying to get the file from S3
+  fetch(`${urlGatewayDeployed}/${bucketOutput}/${name}.json`, { method: "GET"})
+  .then(res => res.json()) // Transform the result to Json
+    .then((json) => {
+      var text = json['results']['transcripts'][0]['transcript']
+      console.log(`Attempt(${tries}): Succeed`)
+      // Reply the message that trigged the event listener with the text
+      return msg.reply(text) 
+    })
+    .catch(err => { // Failed to find the Json on S3 
+      console.log(`Attempt(${tries}): Failed`),
+      tries++
+      return getMethod(tries, name, msg); // Call the function again
+    })
+}
 
-// Going to be executed if there's no ./.wwebjs_auth/ created
 
+// Authentication part
 const client = new Client({authStrategy: new LocalAuth()});
 
 client.on('qr', (qr) => {
-  qrcode.generate(qr, {small:true});
+  qrcode.generate(qr, {small:true}); // Create a qr code on terminal to connect
 });
 
-// Message that sucessfully connected
+// This event listener going to send a message on console when the API connect sucessfuly with the Whatsapp
 client.on('ready', () => {
-  console.log('Client is ready');
-  client.getChats().then(chats=>{
-    // const chat = chats[0]
-    // chat.sendMessage(MessageMedia.fromFilePath("./audio/WhatsappAudio.ogg"))
-  });
+  console.log('Client is ready'); // Just to confirm
 });
 
-client.on('message_create', async (msg)=> {
-  // console.log("I Send a message");
-  let chat = await msg.getChat();
-  
-  if(chat.name == "AudioBot") {
-    if(msg.hasMedia) {
-      // console.log("It has media");
-      let name = "WhatsappAudio-"+ uuidv4()+".ogg";
-      
-      const media = await msg.downloadMedia();
-      
-      const base64data = media.data;
-      const buffer = Buffer.from(base64data.replace('data:audio/ogg; codecs=opus;base64,', ''), 'base64');
-      
-      // Save audio 
-      writeFileSync(`./audio/${name}`, buffer);
-      console.log(`Wrote ${buffer.byteLength.toLocaleString()} bytes to file`)
-      fetch('https://keg43ehf5h.execute-api.us-east-1.amazonaws.com/dev/audio-input-raw/'+name, {
-        method: 'put',
-        body: buffer
-      }).then((res) =>{
-        // console.log('test')
-        // console.log(res);
-      })
+// Intercep any message that the user create
+client.on('message_create', async (msg)=> { 
 
-      // =====================================================================
-      
-      let timeToStop = false;
-      var timer = 20000;
-      var tries = 1;
-      async function getJson(tries) {
-        if(tries > 20) {
-          return console.log("Failed to find the response of AWS");
-        }
-        await new Promise(resolve => setTimeout(resolve, 5000));
-        axios({
-          method: 'get',
-          url: 'https://keg43ehf5h.execute-api.us-east-1.amazonaws.com/dev/transcribe-file-output/'+name+'.json',
-          responseType: 'stream'
+  let chat = await msg.getChat(); // Get in which chat de user send a message
+  
+  if(chat.name == "AudioBot" || chat.name == "Aurilene(31/10)" || chat.name == "FAMILIA Cosmus" || chat.name == "Henrique" || chat.name == "Carla 💩") { // Name of the chat that i want that the API works
+    //(Next Line)Verify if the message that trigged the event has media
+    if(msg.hasMedia) { // This 'hasMedia' refers to Image, Audio, Video
+      if(msg.type == 'audio' || msg.type == 'ptt') { //specify only Audio
+        // Creating a name for the audio
+        let name = "WhatsappAudio-"+ uuidv4()+".ogg";
+        // Downloading the current audio in the msg
+        const media = await msg.downloadMedia(); 
+        
+        // Converting to a buffer so we can send it to AWS service
+        const base64data = media.data;
+        const buffer = Buffer.from(base64data.replace('data:ogg; codecs=opus;base64,', ''), 'base64');
+        
+        // Uploading(PUT) on S3 using api gateway
+        fetch(`${urlGatewayDeployed}/${bucketInput}/${name}`, {
+          method: 'PUT',
+          body: buffer
         })
-        .then(function (response) {
-          // Get Json from the url
-          let url = 'https://keg43ehf5h.execute-api.us-east-1.amazonaws.com/dev/transcribe-file-output/'+name+'.json';
-      
-          let settings = { method: "Get" };
-      
-          fetch(url, settings)
-            .then(res => res.json())
-            .then((json) => {
-              // console.log(json)
-              var text = json['results']['transcripts'][0]['transcript']
-              console.log(`Attempt(${tries}): Succeed`)
-              msg.reply(text)
-              return console.log("The End")
-            })
-            .catch(err => {
-              console.log(`Attempt(${tries}): Failed`),
-              timer=-1000,
-              tries++
-              return getJson(tries);
-            })
-          });
+
+        // Getting(GET) the result of transcribe that is save on S3 bucket
+        // The first parameter is only to track how many tries the function does
+        await getMethod(1, name, msg);
       }
-      await getJson(tries);
       }
     }
   })
